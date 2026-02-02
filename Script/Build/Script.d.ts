1105a7f92b9ad9ac52c312edfd7d4dc5ccaca8d5
JirkaDellOro <del@hs-furtwangen.de>
2026-02-02 13:01:31 +0100

    deleted component script

--- Script/Build/Script.d.ts
+++ Script/Build/Script.d.ts
@@ -1,14 +1,5 @@
 declare namespace Script {
     import ƒ = FudgeCore;
-    class CustomComponentScript extends ƒ.ComponentScript {
-        static readonly iSubclass: number;
-        message: string;
-        constructor();
-        hndEvent: (_event: Event) => void;
-    }
-}
-declare namespace Script {
-    import ƒ = FudgeCore;
     class Texture extends ƒ.Mutable {
         #private;
         normTint: number;
